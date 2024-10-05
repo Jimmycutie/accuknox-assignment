1cfb0038fc0037c8803dc3969f6749758e3d9ac4
Mihir <82092317+Jimmycutie@users.noreply.github.com>
2024-10-05 23:34:53 +0530

    updates

--- src/components/dashboard/add-widget.tsx
+++ src/components/dashboard/add-widget.tsx
@@ -22,52 +22,58 @@ const AddWidget: React.FC<IAddWidget> = ({
   return (
     <>
       <div
-        className={`h-svh w-1/2 z-10 bg-white fixed top-0 right-0 transition-transform duration-500 ${
+        className={`w-svw h-svh fixed top-0 right-0 z-10 bg-black/80 ${
           showSidebar ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className="bg-[#250182] text-white p-3 pl-5 flex justify-between">
-          <div className="flex items-center text-lg">Add Widget</div>
-          <div
-            className="cursor-pointer mb-2"
-            onClick={() => setShowSidebar(false)}
-          >
-            <Close fontSize="small" />
-          </div>
-        </div>
-        <div className="flex w-full justify-between pr-2 pt-2">
-          <h3 className="p-5 py-4">
-            Personalise your Dashboard by adding the following Widget
-          </h3>
-          <Button onClick={setToggleCreateModal}>
-            <p>Create New Widget</p>
-          </Button>
-        </div>
-
-        <div className="flex gap-10 px-10 border-b-2 max-w-max">
-          {options.map((option) => (
+        <div
+          className={`h-svh w-1/2 z-10 bg-white fixed top-0 right-0 transition-transform duration-500 ${
+            showSidebar ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="bg-[#250182] text-white p-3 pl-5 flex justify-between">
+            <div className="flex items-center text-lg">Add Widget</div>
             <div
-              onClick={() => setActive(option)}
-              className={`hover:border-b-2 border-[#2e216e] cursor-pointer pb-4 ${
-                active === option ? "border-b-2" : ""
-              }`}
+              className="cursor-pointer mb-2"
+              onClick={() => setShowSidebar(false)}
             >
-              {option}
+              <Close fontSize="small" />
             </div>
-          ))}
-        </div>
+          </div>
+          <div className="flex w-full justify-between pr-2 pt-2">
+            <h3 className="p-5 py-4">
+              Personalise your Dashboard by adding the following Widget
+            </h3>
+            <Button onClick={setToggleCreateModal}>
+              <p>Create New Widget</p>
+            </Button>
+          </div>
 
-        {categories.map(
-          (category) =>
-            active === category.name.split(" ")[0] &&
-            category.widgets.map((widget) => (
-              <EditWidgets
-                key={widget.id}
-                widget={widget}
-                categoryId={category.id}
-              />
-            ))
-        )}
+          <div className="flex gap-10 px-10 border-b-2 max-w-max">
+            {options.map((option) => (
+              <div
+                onClick={() => setActive(option)}
+                className={`hover:border-b-2 border-[#2e216e] cursor-pointer pb-4 ${
+                  active === option ? "border-b-2" : ""
+                }`}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+
+          {categories.map(
+            (category) =>
+              active === category.name.split(" ")[0] &&
+              category.widgets.map((widget) => (
+                <EditWidgets
+                  key={widget.id}
+                  widget={widget}
+                  categoryId={category.id}
+                />
+              ))
+          )}
+        </div>
       </div>
     </>
   );
--- src/components/dashboard/create-widget.tsx
+++ src/components/dashboard/create-widget.tsx
@@ -49,54 +49,62 @@ const CreateWidget: React.FC<ICreateWidget> = ({
   return (
     <>
       <div
-        className={`h-max w-1/2 z-20 bg-white fixed top-1/3 left-1/4 transition-transform duration-500${
-          toggleCreateModal ? "-translate-x-0" : "-translate-x-full"
+        className={`w-svw h-svh fixed top-0 right-0 z-20 bg-black/80 ${
+          toggleCreateModal ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className="h-[350px] w-full flex flex-col gap-5 p-5 border border-[gray]">
-          <div className="flex w-full justify-between">
-            <p className="font-bold p-3 bg-[#040468] text-[#fff]">Add Widget</p>
-            <Button onClick={setToggleCreateModal}>
-              <Close fontSize="small" />
-            </Button>
-          </div>
+        <div
+          className={`h-max w-1/2 z-20 bg-white fixed top-1/3 left-1/4 transition-transform duration-500${
+            toggleCreateModal ? "-translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="h-[350px] w-full flex flex-col gap-5 p-5 border border-[gray]">
+            <div className="flex w-full justify-between">
+              <p className="font-bold p-3 bg-[#040468] text-[#fff]">
+                Add Widget
+              </p>
+              <Button onClick={setToggleCreateModal}>
+                <Close fontSize="small" />
+              </Button>
+            </div>
 
-          <select
-            defaultValue={"Select Category"}
-            value={selectedCategoryId || "Select Category"}
-            onChange={(e) => {
-              console.log(e.target.value);
-              return setSelectedCategoryId(e.target.value);
-            }}
-            className="border border-[gray] p-2 rounded-md bg-[#fff]"
-          >
-            <option disabled>Select Category</option>
-            {categories?.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-          <input
-            placeholder="Enter widget name"
-            value={widgetName}
-            onChange={(e) => setWidgetName(e.target.value)}
-            className="border border-[gray] p-2 rounded-md bg-[#fff]"
-          />
-          <input
-            placeholder="Enter widget text"
-            value={widgetText}
-            onChange={(e) => setWidgetText(e.target.value)}
-            className="border border-[gray] p-2 rounded-md bg-[#fff]"
-          />
-        </div>
-        <div className="absolute right-2 bottom-2">
-          <button
-            className="bg-[#01123d] text-[#fff] rounded-md p-2.5 ml-2"
-            onClick={handleWidgetAddition}
-          >
-            Save
-          </button>
+            <select
+              defaultValue={"Select Category"}
+              value={selectedCategoryId || "Select Category"}
+              onChange={(e) => {
+                console.log(e.target.value);
+                return setSelectedCategoryId(e.target.value);
+              }}
+              className="border border-[gray] p-2 rounded-md bg-[#fff]"
+            >
+              <option disabled>Select Category</option>
+              {categories?.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <input
+              placeholder="Enter widget name"
+              value={widgetName}
+              onChange={(e) => setWidgetName(e.target.value)}
+              className="border border-[gray] p-2 rounded-md bg-[#fff]"
+            />
+            <input
+              placeholder="Enter widget text"
+              value={widgetText}
+              onChange={(e) => setWidgetText(e.target.value)}
+              className="border border-[gray] p-2 rounded-md bg-[#fff]"
+            />
+          </div>
+          <div className="absolute right-2 bottom-2">
+            <button
+              className="bg-[#01123d] text-[#fff] rounded-md p-2.5 ml-2"
+              onClick={handleWidgetAddition}
+            >
+              Save
+            </button>
+          </div>
         </div>
       </div>
     </>
